7923ee806cc40c0a239035f9da7c688fcca3eb1d
shravanGT <gtatalex@gmail.com>
2024-12-23 17:30:12 +0530

    Fix the bugs

--- script.js
+++ script.js
@@ -1,78 +1,74 @@
-let computerScore = 0;
-let humanScore = 0;
-let gameCount = 0;
-let playerselectionButton = document.querySelectorAll(".buttons")
-let display = document.querySelector('.display');
-let displayResult = document.createElement('h2');
-displayResult.classList.add('display-text');
-let displayScorecard = document.createElement('h2');
-displayScorecard.classList.add('display-text')
-displayScorecard.classList.add('score-text')
-let container = document.querySelector('.container');
-let resetButton = document.createElement('button')
-resetButton.classList.add('resetButt')
-let gamecount = document.querySelector('.game-count')
-gamecount.textContent = 'Game:0'
+document.addEventListener('DOMContentLoaded', () => {
 
+    let computerScore = 0;
+    let humanScore = 0;
+    let gameCount = 0;
+    let playerselectionButton = document.querySelectorAll(".buttons")
+    let display = document.querySelector('.display');
+    let displayResult = document.createElement('h2');
+    displayResult.classList.add('display-text');
+    let displayScorecard = document.createElement('h2');
+    displayScorecard.classList.add('display-text')
+    let resetButton = document.createElement('button')
+    resetButton.classList.add('resetButt')
+    let gamecount = document.querySelector('.game-count')
 
-function getComputerChoice() {
-    let number = Math.round(Math.random() * 10);
-    let compChoice;
-    if (number > 0 && number < 3.5) {
-        compChoice = "rock"
-    } else if (number > 3.5 && number < 7) {
-        compChoice = "paper"
-    } else {
-        compChoice = "scissor"
+    function getComputerChoice() {
+        let number = Math.round(Math.random() * 10);
+        let compChoice;
+        if (number > 0 && number < 3.5) {
+            compChoice = "rock"
+        } else if (number > 3.5 && number < 7) {
+            compChoice = "paper"
+        } else {
+            compChoice = "scissor"
+        }
+        return compChoice;
     }
-    return compChoice;
-}
 
-function getHumanChoice(e) {
-    return e.target.classList.value;
-}
+    function getHumanChoice(e) {
+        return e.target.classList.value;
+    }
 
-function playRound(compChoice, humanChoice) {
-    let result = '';
-    let scoreCard = '';
-    if (compChoice === humanChoice) {
-        result = 'Result: Draw'
-        scoreCard = `ScoreBoard: you- ${humanScore} computer- ${computerScore}`
-    } else if (compChoice === "rock" && humanChoice === "scissor" || compChoice === "scissor" && humanChoice === "paper" || compChoice === "paper" && humanChoice === "rock") {
-        result = 'Result:Lose'
-        computerScore++;
-        scoreCard = `ScoreBoard:
+    function playRound(compChoice, humanChoice) {
+        let result = '';
+        let scoreCard = '';
+        if (compChoice === humanChoice) {
+            result = 'Result: Draw'
+            scoreCard = `ScoreBoard: you- ${humanScore} computer- ${computerScore}`
+        } else if (compChoice === "rock" && humanChoice === "scissor" || compChoice === "scissor" && humanChoice === "paper" || compChoice === "paper" && humanChoice === "rock") {
+            result = 'Result:Lose'
+            computerScore++;
+            scoreCard = `ScoreBoard:
         you- ${humanScore} computer- ${computerScore}`
-    } else if (humanChoice === "rock" && compChoice === "scissor" || humanChoice === "scissor" && compChoice === "paper" || humanChoice === "paper" && compChoice === "rock") {
-        result = 'Result: Win'
-        humanScore++;
-        scoreCard = `ScoreBoard: you- ${humanScore} computer- ${computerScore}`
-    } else {
-        result = 'error-choice again and check it carefully'
+        } else if (humanChoice === "rock" && compChoice === "scissor" || humanChoice === "scissor" && compChoice === "paper" || humanChoice === "paper" && compChoice === "rock") {
+            result = 'Result: Win'
+            humanScore++;
+            scoreCard = `ScoreBoard: you- ${humanScore} computer- ${computerScore}`
+        } else {
+            result = 'error-choice again and check it carefully'
+        }
+        displayResult.textContent = result;
+        displayScorecard.textContent = scoreCard;
+        gameCount++;
+        display.appendChild(displayResult)
+        display.appendChild(displayScorecard)
     }
-    displayResult.textContent = result;
-    displayScorecard.textContent = scoreCard;
-    gameCount++;
-    display.appendChild(displayResult)
-    display.appendChild(displayScorecard)
-}
-let count = 0;
-let result = ''
-let score = ''
-playerselectionButton.forEach(button => {
-    button.addEventListener("click", (e) => {
-        let humanChoice = getHumanChoice(e);
-        let compChoice = getComputerChoice();
-        playRound(compChoice, humanChoice);
-        gamecount.textContent = (`Game:${gameCount}`)
-        resetButton.textContent = 'reset'
-        resetButton.addEventListener('click', () => {
-            humanScore = 0;
-            computerScore = 0;
-            display.innerHTML = ''
-            gameCount = 0;
-            gamecount.innerHTML = `Game:${gameCount}`
+    playerselectionButton.forEach(button => {
+        button.addEventListener("click", (e) => {
+            let humanChoice = getHumanChoice(e);
+            let compChoice = getComputerChoice();
+            playRound(compChoice, humanChoice);
+            gamecount.textContent = (`Game:${gameCount}`)
+            resetButton.textContent = 'reset'
+            resetButton.addEventListener('click', () => {
+                humanScore = 0;
+                computerScore = 0;
+                display.innerHTML = ''
+                gameCount = 0;
+                gamecount.innerHTML = `Game:${gameCount}`
+            })
+            display.appendChild(resetButton)
         })
-        display.appendChild(resetButton)
     })
 })
