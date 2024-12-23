af74fbd8a7a0de5cd7c406e243e509272fbc8404
shravanGT <gtatalex@gmail.com>
2024-12-23 16:33:59 +0530

    Add the UI feature and design
    add the styling with responsive design

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let computerScore = 0;
 let humanScore = 0;
+let gameCount = 0;
+let playerselectionButton = document.querySelectorAll(".buttons")
+let display = document.querySelector('.display');
+let displayResult = document.createElement('h2');
+displayResult.classList.add('display-text');
+let displayScorecard = document.createElement('h2');
+displayScorecard.classList.add('display-text')
+displayScorecard.classList.add('score-text')
+let container = document.querySelector('.container');
+let resetButton = document.createElement('button')
+resetButton.classList.add('resetButt')
+let gamecount = document.querySelector('h2')
+gamecount.classList.add('GameCount')
+gamecount.style.color = 'white'
 
 function getComputerChoice() {
     let number = Math.round(Math.random() * 10);
-
     let compChoice;
     if (number > 0 && number < 3.5) {
         compChoice = "rock"
@@ -13,66 +26,53 @@ function getComputerChoice() {
         compChoice = "scissor"
     }
     return compChoice;
-
 }
 
-
-function getHumanChoice() {
-    let humanChoice = String(prompt("State your choice-Rock ,Paper or Scissor")).toLowerCase();
-    return humanChoice
+function getHumanChoice(e) {
+    return e.target.classList.value;
 }
 
 function playRound(compChoice, humanChoice) {
-    if (compChoice === "rock" && humanChoice === "rock" || compChoice === "paper" && humanChoice === "paper" || compChoice === "scissor" && humanChoice === "scissor") {
-        alert(`It's draw!! your choice ${humanChoice} and computer choice ${compChoice} are same`);
-        console.log(`ScoreBoard you- ${humanScore} computer- ${computerScore}`);
-        alert(`ScoreBoard you- ${humanScore} computer- ${computerScore}`);
-
+    let result = '';
+    let scoreCard = '';
+    if (compChoice === humanChoice) {
+        result = 'Result: Draw'
+        scoreCard = `ScoreBoard: you- ${humanScore} computer- ${computerScore}`
     } else if (compChoice === "rock" && humanChoice === "scissor" || compChoice === "scissor" && humanChoice === "paper" || compChoice === "paper" && humanChoice === "rock") {
-        alert(`You Lose! ${compChoice} defeats ${humanChoice}`)
+        result = 'Result:Lose'
         computerScore++;
-        console.log(`ScoreBoard you- ${humanScore} computer- ${computerScore}`);
-        alert(`ScoreBoard you- ${humanScore} computer- ${computerScore}`);
-
+        scoreCard = `ScoreBoard:
+        you- ${humanScore} computer- ${computerScore}`
     } else if (humanChoice === "rock" && compChoice === "scissor" || humanChoice === "scissor" && compChoice === "paper" || humanChoice === "paper" && compChoice === "rock") {
-        alert(`You Won! ${humanChoice} defeats ${compChoice}`)
+        result = 'Result: Win'
         humanScore++;
-        console.log(`ScoreBoard you- ${humanScore} computer- ${computerScore}`);
-        alert(`ScoreBoard you- ${humanScore} computer- ${computerScore}`);
+        scoreCard = `ScoreBoard: you- ${humanScore} computer- ${computerScore}`
     } else {
-        console.log('error-choice again and check it carefully')
-        alert('error-choice again and check it carefully')
-
+        result = 'error-choice again and check it carefully'
     }
+    displayResult.textContent = result;
+    displayScorecard.textContent = scoreCard;
+    gameCount++;
+    display.appendChild(displayResult)
+    display.appendChild(displayScorecard)
 }
-
-function playGame() {
-    let count = 1;
-    while (count < 6) {
-        // To play 5 Rounds
-        let humanChoice = getHumanChoice()
-        let compChoice = getComputerChoice()
-
-        if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissor") {
-            playRound(compChoice, humanChoice)
-            count++;
-        } else {
-            alert("enter correctly")
-            break;
-        }
-
-
-    }
-    // Declare final winner
-    if (humanScore > computerScore) {
-        console.log('You Won')
-        alert(`You won by ${humanScore-computerScore} points`);
-    } else if (humanScore < computerScore) {
-        console.log('You Lose')
-        alert(`You lost by ${computerScore-humanScore} points`);
-    } else {
-        console.log('Draw')
-        alert(`Draw!! ScoreBoard - ${humanScore} ${computerScore} points`);
-    }
-}
-playGame();
+let count = 0;
+let result = ''
+let score = ''
+playerselectionButton.forEach(button => {
+    button.addEventListener("click", (e) => {
+        let humanChoice = getHumanChoice(e);
+        let compChoice = getComputerChoice();
+        playRound(compChoice, humanChoice);
+        gamecount.textContent = (`Game:${gameCount}`)
+        resetButton.textContent = 'reset'
+        resetButton.addEventListener('click', () => {
+            humanScore = 0;
+            computerScore = 0;
+            display.innerHTML = ''
+            gameCount = 0;
+            gamecount.innerHTML = `Game:${gameCount}`
+        })
+        display.appendChild(resetButton)
+    })
+})
